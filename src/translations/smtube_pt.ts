5d9b2ded19a7955d085b6cd751a24a2d4d07001f
smarquespt <smarquespt@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-03-25 01:36:57 +0000

    Added Portuguese translation of smtube

---
+++ src/translations/smtube_pt.ts
@@ -0,0 +1,519 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../about.ui" line="14"/>
+        <source>About...</source>
+        <translation>Sobre...</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="33"/>
+        <source>&amp;Info</source>
+        <translation>&amp;Informações</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="53"/>
+        <location filename="../about.ui" line="109"/>
+        <location filename="../about.ui" line="165"/>
+        <source>icon</source>
+        <translation>ícone</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="89"/>
+        <source>&amp;Translators</source>
+        <translation>&amp;Tradutores</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="145"/>
+        <source>&amp;License</source>
+        <translation>&amp;Licença</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="37"/>
+        <source>YouTube%1 Browser for SMPlayer</source>
+        <translation>Explorador YouTube %1 para o SMPlayer</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="39"/>
+        <source>Based on UMPlayer</source>
+        <translation>Inspirado pelo UMPlayer</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="41"/>
+        <source>Version: %1</source>
+        <translation>Versão: %1</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="43"/>
+        <source>Portable Edition</source>
+        <translation>Edição portátil</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="46"/>
+        <source>Compiled with Qt %1 (using %2)</source>
+        <translation>Compilado com Qt %1 (a utilizar %2)</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="48"/>
+        <source>Visit our web for updates:</source>
+        <translation>Consulte a página web para atualizações:</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="95"/>
+        <source>The following people have contributed with translations:</source>
+        <translation>As seguintes pessoas contribuiram com traduções:</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="101"/>
+        <source>Spanish</source>
+        <translation>Espanhol</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="102"/>
+        <source>Russian</source>
+        <translation>Russo</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="103"/>
+        <source>Lithuanian</source>
+        <translation>Lituano</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="104"/>
+        <source>Japanese</source>
+        <translation>Japonês</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="105"/>
+        <source>Simplified-Chinese</source>
+        <translation>Mandarim simplificado</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="106"/>
+        <source>Basque</source>
+        <translation>Basco</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="123"/>
+        <source>%1 and %2</source>
+        <translation>%1 e %2</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="124"/>
+        <source>%1, %2 and %3</source>
+        <translation>%1, %2 e %3</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="125"/>
+        <source>%1, %2, %3 and %4</source>
+        <translation>%1, %2, %3 e %4</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="126"/>
+        <source>%1, %2, %3, %4 and %5</source>
+        <translation>%1, %2, %3, %4 e %5</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="135"/>
+        <location filename="../about.cpp" line="146"/>
+        <source>&lt;b&gt;%1&lt;/b&gt;: %2</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt;: %2</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="167"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; (%2)</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; (%2)</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigDialog</name>
+    <message>
+        <location filename="../configdialog.ui" line="14"/>
+        <source>Settings</source>
+        <translation>Definições</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="20"/>
+        <source>Recording</source>
+        <translation>Gravação</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="28"/>
+        <source>&amp;Save to this folder</source>
+        <translation>&amp;Gravar nesta pasta</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="45"/>
+        <source>&amp;Quality</source>
+        <translation>&amp;Qualidade</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="59"/>
+        <location filename="../configdialog.cpp" line="113"/>
+        <source>Normal (360p)</source>
+        <translation>Normal (360p)</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="64"/>
+        <location filename="../configdialog.cpp" line="113"/>
+        <source>HD (720p)</source>
+        <translation>HD (720p)</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="69"/>
+        <location filename="../configdialog.cpp" line="113"/>
+        <source>Full HD (1080p)</source>
+        <translation>Full HD (1080p)</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="90"/>
+        <source>&amp;Format</source>
+        <translation>&amp;Formato</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="101"/>
+        <source>MP4</source>
+        <translation>MP4</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="106"/>
+        <source>Flv</source>
+        <translation>Flv</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.cpp" line="127"/>
+        <source>Low Quality (226p)</source>
+        <translation>Baixa qualidade</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.cpp" line="127"/>
+        <source>Medium Quality (360p)</source>
+        <translation>Qualidade média (360p)</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.cpp" line="127"/>
+        <source>High Quality (480p)</source>
+        <translation>Alta qualidade (480p)</translation>
+    </message>
+</context>
+<context>
+    <name>DownloadFile</name>
+    <message>
+        <location filename="../downloadfile.cpp" line="115"/>
+        <source>%1 hours</source>
+        <translation>%1 horas</translation>
+    </message>
+    <message>
+        <location filename="../downloadfile.cpp" line="121"/>
+        <source>%1 minutes</source>
+        <translation>%1 minutos</translation>
+    </message>
+    <message>
+        <location filename="../downloadfile.cpp" line="108"/>
+        <source>unknown time remaining</source>
+        <translation>tempo remanescente desconhecido</translation>
+    </message>
+    <message>
+        <location filename="../downloadfile.cpp" line="124"/>
+        <source>%1 seconds</source>
+        <translation>%1 segundos</translation>
+    </message>
+    <message>
+        <location filename="../downloadfile.cpp" line="125"/>
+        <source>%1 remaining</source>
+        <translation>faltam %1</translation>
+    </message>
+    <message>
+        <location filename="../downloadfile.cpp" line="130"/>
+        <source>%1 of %2 MB</source>
+        <translation>%1 de %2 MB</translation>
+    </message>
+    <message>
+        <location filename="../downloadfile.cpp" line="136"/>
+        <source>%1 of %2 KB</source>
+        <translation>%1 de %2 KB</translation>
+    </message>
+    <message>
+        <location filename="../downloadfile.cpp" line="142"/>
+        <source>%1 of %2 bytes</source>
+        <translation>%1 de %2 bytes</translation>
+    </message>
+    <message>
+        <location filename="../downloadfile.cpp" line="146"/>
+        <source>%1 of unknown size</source>
+        <translation>%1 de tamanho desconhecido</translation>
+    </message>
+    <message>
+        <location filename="../downloadfile.cpp" line="151"/>
+        <source>%1 MB/sec</source>
+        <translation>%1 MB/seg</translation>
+    </message>
+    <message>
+        <location filename="../downloadfile.cpp" line="155"/>
+        <source>%1 KB/sec</source>
+        <translation>%1 KB/seg</translation>
+    </message>
+    <message>
+        <location filename="../downloadfile.cpp" line="159"/>
+        <source>%1 bytes/sec</source>
+        <translation>%1 bytes/seg</translation>
+    </message>
+</context>
+<context>
+    <name>FileChooser</name>
+    <message>
+        <location filename="../filechooser.cpp" line="54"/>
+        <source>Click to select a file or folder</source>
+        <translation>Clique para escolher o ficheiro ou pasta</translation>
+    </message>
+</context>
+<context>
+    <name>RecordingDelegate</name>
+    <message>
+        <location filename="../recordingdialog.cpp" line="120"/>
+        <source>Canceled</source>
+        <translation>Cancelada</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="120"/>
+        <source>Error</source>
+        <translation>Erro</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="150"/>
+        <source>%1 MB</source>
+        <translation>%1 MB</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="154"/>
+        <source>%1 KB</source>
+        <translation>%1 KB</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="158"/>
+        <source>%1 bytes</source>
+        <translation>%1 bytes</translation>
+    </message>
+</context>
+<context>
+    <name>RecordingDialog</name>
+    <message>
+        <location filename="../recordingdialog.cpp" line="202"/>
+        <location filename="../recordingdialog.cpp" line="415"/>
+        <source>Recordings</source>
+        <translation>Gravações</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="203"/>
+        <source>&amp;Clear list</source>
+        <translation>Apagar &amp;lista</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="204"/>
+        <location filename="../recordingdialog.cpp" line="544"/>
+        <source>&amp;Open folder</source>
+        <translation>&amp;Abrir pasta</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="285"/>
+        <location filename="../recordingdialog.cpp" line="680"/>
+        <source>There was an error in retrieving the download URL.</source>
+        <translation>Ocorreu um erro ao obter o URL da transferência.</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="285"/>
+        <location filename="../recordingdialog.cpp" line="680"/>
+        <source>Recording failed</source>
+        <translation>Falha ao gravar</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="303"/>
+        <location filename="../recordingdialog.cpp" line="698"/>
+        <source>Starting</source>
+        <translation>A iniciar</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="336"/>
+        <source>Error</source>
+        <translation>Erro</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="340"/>
+        <source>Completed</source>
+        <translation>Terminada</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../recordingdialog.cpp" line="419"/>
+        <source>%1% of %n video - Recordings</source>
+        <translation>
+            <numerusform>%1% de %n vídeo - Gravações</numerusform>
+            <numerusform>%1% de %n vídeos - Gravações</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="504"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Cancelar</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="506"/>
+        <source>&amp;Remove from list</source>
+        <translation>&amp;Remover da lista</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="525"/>
+        <location filename="../recordingdialog.cpp" line="547"/>
+        <source>Remove from &amp;list</source>
+        <translation>Remover da &amp;lista</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="523"/>
+        <source>&amp;Retry</source>
+        <translation>&amp;Tentar novamente</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="543"/>
+        <source>&amp;Play</source>
+        <translation>&amp;Reproduzir</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="546"/>
+        <source>&amp;Delete</source>
+        <translation>&amp;Eliminar</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="625"/>
+        <source>Fetching URL...</source>
+        <translation>A obter URL...</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="640"/>
+        <source>File error</source>
+        <translation>Erro no ficheiro</translation>
+    </message>
+    <message>
+        <location filename="../recordingdialog.cpp" line="641"/>
+        <source>The file does not exists, would you like to remove it from the list?</source>
+        <translation>O ficheiro não existe. Prtende remover o ficheiro da lista?</translation>
+    </message>
+</context>
+<context>
+    <name>YTDelegate</name>
+    <message>
+        <location filename="../ytdelegate.cpp" line="114"/>
+        <source>%L1 views</source>
+        <translation>%L1 visualizações</translation>
+    </message>
+</context>
+<context>
+    <name>YTDialog</name>
+    <message>
+        <location filename="../ytdialog.cpp" line="211"/>
+        <source>YouTube Browser - SMPlayer</source>
+        <translation>Explorador YouTube - SMPlayer</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="234"/>
+        <source>&amp;Next</source>
+        <translation>Segui&amp;nte</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="238"/>
+        <source>&amp;Previous</source>
+        <translation>An&amp;terior</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="249"/>
+        <source>Settings</source>
+        <translation>Definições</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="254"/>
+        <source>Information</source>
+        <translation>Informações</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="389"/>
+        <source>Most Relevant</source>
+        <translation>Mais relevantes</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="393"/>
+        <source>Most Recent</source>
+        <translation>Mais recentes</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="397"/>
+        <location filename="../ytdialog.cpp" line="405"/>
+        <source>Most Viewed</source>
+        <translation>Mais visualizações</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="401"/>
+        <source>Most Popular</source>
+        <translation>Mais populares</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="409"/>
+        <source>Most Rated</source>
+        <translation>Vídeos com melhor avaliação</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="420"/>
+        <source>Most popular videos on YouTube%1</source>
+        <translation>Vídeos mais populares no YouTube %1</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="421"/>
+        <source>Most rated videos on YouTube%1</source>
+        <translation>Vídeos com melhor avaliação no YouTube %1</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="422"/>
+        <source>Most viewed videos on YouTube%1</source>
+        <translation>Vídeos com mais visualizações no YouTube %1</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="536"/>
+        <source>Error: Could not connect to Youtube server.</source>
+        <translation>Erro: não foi possível ligar ao servidor YouTube.</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="425"/>
+        <source>YouTube%2 results for &quot;%1&quot;</source>
+        <translation>Resultados no YouTube %2 sobre &quot;%1&quot;</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="542"/>
+        <source>No videos found</source>
+        <translation>Nenhum vídeo encontrado</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="544"/>
+        <source>No videos found for &quot;%1&quot;</source>
+        <translation>Nenhum vídeo encontrado para &quot;%1&quot;</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="620"/>
+        <source>&amp;Play video</source>
+        <translation>&amp;Reproduzir vídeo</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="621"/>
+        <source>&amp;Record video</source>
+        <translation>&amp;Gravar vídeo</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="622"/>
+        <source>&amp;Watch on YouTube</source>
+        <translation>&amp;Ver no YouTube</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="623"/>
+        <source>&amp;Copy link</source>
+        <translation>&amp;Copiar ligação</translation>
+    </message>
+</context>
+</TS>
