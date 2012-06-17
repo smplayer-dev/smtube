0bb6af42e01a90d736cf026ecf8681733b9a5949
redxii <redxii@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-06-17 20:42:30 +0000

    Clean more stuff left behind from build process in smtube

--- clean_windows.bat
+++ clean_windows.bat
@@ -3,3 +3,8 @@ call mingw32-make distclean
 
 cd ..
 del src\translations\smtube_*.qm
+del src\object_script.smtube.Release
+del src\object_script.smtube.Debug
+del src\svn_revision.h
+rd src\release
+rd src\debug
