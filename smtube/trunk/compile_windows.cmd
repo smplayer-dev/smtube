a0e1b00bc6672f3b4d91fe9656b9fa6b5e479791
redxii <redxii@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-12-23 01:10:29 +0000

    Properly check for errorlevel, reset to 0 and convert tabs to spaces

--- smtube/trunk/compile_windows.cmd
+++ smtube/trunk/compile_windows.cmd
@@ -6,27 +6,27 @@ set qmake_defs=
 :arg_loop2
 if [%1]==[] (
 
-	goto compile
+  goto compile
 
 ) else if [%1]==[pe] (
 
-	set qmake_defs="DEFINES+=PORTABLE_APP"
+  set qmake_defs="DEFINES+=PORTABLE_APP"
 
 ) else if [%1]==[-h] (
 
-	echo How to use:
-	echo.
-	echo Add ^`pe^' to compile portable.
-	echo.
-	echo To compile SMTube non-portable, enter no arguments.
-	echo.
-	echo ex: %script_name% pe
+  echo How to use:
+  echo.
+  echo Add ^`pe^' to compile portable.
+  echo.
+  echo To compile SMTube non-portable, enter no arguments.
+  echo.
+  echo ex: %script_name% pe
 
 ) else (
 
-	echo configure: error: unrecognized option: `%1'
-	echo Try `%script_name% -h' for more information
-	goto end
+  echo configure: error: unrecognized option: `%1'
+  echo Try `%script_name% -h' for more information
+  goto end
 
 )
 
