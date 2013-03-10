ab04ec15c8b6ad69b61fb81e50bdb0116f6dbb06
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2013-03-10 01:15:27 +0000

    Update the basque translation

--- src/translations/smtube_eu.ts
+++ src/translations/smtube_eu.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="eu">
+<!DOCTYPE TS><TS version="1.1" language="eu">
 <context>
     <name>About</name>
     <message>
@@ -14,8 +13,6 @@
         <translation>&amp;Argibideak</translation>
     </message>
     <message>
-        <location filename="../about.ui" line="53"/>
-        <location filename="../about.ui" line="109"/>
         <location filename="../about.ui" line="165"/>
         <source>icon</source>
         <translation>ikurra</translation>
@@ -33,7 +30,7 @@
     <message>
         <location filename="../about.cpp" line="33"/>
         <source>YouTube%1 Browser for SMPlayer</source>
-        <translation>YouTube%1 Bilatzailea SM Irakurgailuarentzat</translation>
+        <translation>YouTube%1 Nabigatzailea SM Irakurgailuarentzat</translation>
     </message>
     <message>
         <location filename="../about.cpp" line="35"/>
@@ -121,22 +118,27 @@
         <translation>Hebraiera</translation>
     </message>
     <message>
+        <location filename="../about.cpp" line="120"/>
         <source>%1 and %2</source>
         <translation type="obsolete">%1 eta %2</translation>
     </message>
     <message>
+        <location filename="../about.cpp" line="120"/>
         <source>%1, %2 and %3</source>
         <translation type="obsolete">%1, %2 eta %3</translation>
     </message>
     <message>
+        <location filename="../about.cpp" line="120"/>
         <source>%1, %2, %3 and %4</source>
         <translation type="obsolete">%1, %2, %3 eta %4</translation>
     </message>
     <message>
+        <location filename="../about.cpp" line="120"/>
         <source>%1, %2, %3, %4 and %5</source>
         <translation type="obsolete">%1, %2, %3, %4 eta %5</translation>
     </message>
     <message>
+        <location filename="../about.cpp" line="120"/>
         <source>&lt;b&gt;%1&lt;/b&gt;: %2</source>
         <translation type="obsolete">&lt;b&gt;%1&lt;/b&gt;: %2</translation>
     </message>
@@ -189,19 +191,16 @@
         <translation>&amp;Ontasuna</translation>
     </message>
     <message>
-        <location filename="../configdialog.ui" line="59"/>
         <location filename="../configdialog.cpp" line="117"/>
         <source>Normal (360p)</source>
         <translation>Arrunta (360p)</translation>
     </message>
     <message>
-        <location filename="../configdialog.ui" line="64"/>
         <location filename="../configdialog.cpp" line="117"/>
         <source>HD (720p)</source>
         <translation>HD (720p)</translation>
     </message>
     <message>
-        <location filename="../configdialog.ui" line="69"/>
         <location filename="../configdialog.cpp" line="117"/>
         <source>Full HD (1080p)</source>
         <translation>Full HD (1080p)</translation>
@@ -262,7 +261,7 @@
     <message>
         <location filename="../downloadfile.cpp" line="108"/>
         <source>unknown time remaining</source>
-        <translation>ezezaguna gelditzen den denbora</translation>
+        <translation>gelditzen den denbora ezezaguna</translation>
     </message>
     <message>
         <location filename="../downloadfile.cpp" line="124"/>
@@ -349,7 +348,6 @@
 <context>
     <name>RecordingDialog</name>
     <message>
-        <location filename="../recordingdialog.cpp" line="204"/>
         <location filename="../recordingdialog.cpp" line="417"/>
         <source>Recordings</source>
         <translation>Grabaketak</translation>
@@ -360,25 +358,21 @@
         <translation>&amp;Garbitu zerrenda</translation>
     </message>
     <message>
-        <location filename="../recordingdialog.cpp" line="206"/>
         <location filename="../recordingdialog.cpp" line="546"/>
         <source>&amp;Open folder</source>
         <translation>Ireki &amp;agiritegia</translation>
     </message>
     <message>
-        <location filename="../recordingdialog.cpp" line="287"/>
         <location filename="../recordingdialog.cpp" line="682"/>
         <source>There was an error in retrieving the download URL.</source>
         <translation>Akats bat gertatu da jeisketa URL gertatzerakoan.</translation>
     </message>
     <message>
-        <location filename="../recordingdialog.cpp" line="287"/>
         <location filename="../recordingdialog.cpp" line="682"/>
         <source>Recording failed</source>
         <translation>Grabaketa hutsegitea</translation>
     </message>
     <message>
-        <location filename="../recordingdialog.cpp" line="305"/>
         <location filename="../recordingdialog.cpp" line="700"/>
         <source>Starting</source>
         <translation>Abiarazten</translation>
@@ -412,7 +406,6 @@
         <translation>&amp;Kendu zerrendatik</translation>
     </message>
     <message>
-        <location filename="../recordingdialog.cpp" line="527"/>
         <location filename="../recordingdialog.cpp" line="549"/>
         <source>Remove from &amp;list</source>
         <translation>Kendu ze&amp;rrendatik</translation>
@@ -461,13 +454,15 @@
     <message>
         <location filename="../ytdialog.cpp" line="210"/>
         <source>YouTube Browser - SMPlayer</source>
-        <translation>YouTube Bilatzailea - SM Irakurgailuarentzat</translation>
+        <translation>YouTube Nabigatzailea - SM Irakurgailuarentzat</translation>
     </message>
     <message>
+        <location filename="../ytdialog.cpp" line="210"/>
         <source>&amp;Next</source>
         <translation type="obsolete">&amp;Hurrengoa</translation>
     </message>
     <message>
+        <location filename="../ytdialog.cpp" line="210"/>
         <source>&amp;Previous</source>
         <translation type="obsolete">A&amp;urrekoa</translation>
     </message>
@@ -509,7 +504,7 @@
     <message>
         <location filename="../ytdialog.cpp" line="451"/>
         <source>Recently Featured</source>
-        <translation type="unfinished"></translation>
+        <translation>Azken Aurkezpenak</translation>
     </message>
     <message>
         <location filename="../ytdialog.cpp" line="455"/>
@@ -527,6 +522,7 @@
         <translation>Bideo mailatuenak YouTube%1-ean</translation>
     </message>
     <message>
+        <location filename="../ytdialog.cpp" line="467"/>
         <source>Most viewed videos on YouTube%1</source>
         <translation type="obsolete">Bideo ikusienak YouTube%1-ean</translation>
     </message>
@@ -543,7 +539,7 @@
     <message>
         <location filename="../ytdialog.cpp" line="468"/>
         <source>Videos recently featured on YouTube%1</source>
-        <translation type="unfinished"></translation>
+        <translation>YouTube%1-ko azken bideo aurkezpenak</translation>
     </message>
     <message>
         <location filename="../ytdialog.cpp" line="595"/>
