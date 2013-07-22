a8212812f404e1f805e322db7100869e4fb3dd66
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2013-07-22 00:19:12 +0000

    Download the script from another server

--- smtube/trunk/src/ytdialog.cpp
+++ smtube/trunk/src/ytdialog.cpp
@@ -787,7 +787,7 @@ void YTDialog::showErrorSignatureNotFound(const QString & title) {
 		if (!downloader) downloader = new CodeDownloader(this);
 		downloader->saveAs(script_file);
 		downloader->show();
-		downloader->download(QUrl("http://smplayer.sourceforge.net/ytcode.script"));
+		downloader->download(QUrl("http://updates.smplayer.info/ytcode.script"));
 	}
 	#else
 	QMessageBox::warning(this, tr("Problems with Youtube"),
