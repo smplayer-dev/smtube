eb0508b952ecd56ac0a49e3c43902d74df175b97
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2015-05-08 23:48:05 +0000

    Update translations

--- smtube/trunk/src/translations/smtube_es.ts
+++ smtube/trunk/src/translations/smtube_es.ts
@@ -4,64 +4,145 @@
 <context>
     <name>BrowserWindow</name>
     <message>
-        <location filename="../browserwindow.cpp" line="88"/>
+        <location filename="../browserwindow.cpp" line="91"/>
         <source>Home</source>
         <translation>Inicio</translation>
     </message>
     <message>
-        <location filename="../browserwindow.cpp" line="91"/>
+        <location filename="../browserwindow.cpp" line="94"/>
         <source>Navigation</source>
         <translation>Navegación</translation>
     </message>
     <message>
-        <location filename="../browserwindow.cpp" line="99"/>
+        <location filename="../browserwindow.cpp" line="102"/>
         <source>&amp;View</source>
         <translation>&amp;Ver</translation>
     </message>
     <message>
-        <location filename="../browserwindow.cpp" line="100"/>
+        <location filename="../browserwindow.cpp" line="103"/>
         <source>Toolbar</source>
         <translation>Barra de herramientas</translation>
     </message>
     <message>
-        <location filename="../browserwindow.cpp" line="104"/>
+        <location filename="../browserwindow.cpp" line="107"/>
         <source>Statusbar</source>
         <translation>Barra de estado</translation>
     </message>
     <message>
         <location filename="../browserwindow.cpp" line="111"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Configuración</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="118"/>
         <source>&amp;Help</source>
         <translation>&amp;Ayuda</translation>
     </message>
     <message>
-        <location filename="../browserwindow.cpp" line="112"/>
+        <location filename="../browserwindow.cpp" line="119"/>
         <source>About</source>
         <translation>Acerca de</translation>
     </message>
     <message>
-        <location filename="../browserwindow.cpp" line="177"/>
+        <location filename="../browserwindow.cpp" line="184"/>
         <source>Loading... %1%</source>
         <translation>Cargando... %1%</translation>
     </message>
     <message>
-        <location filename="../browserwindow.cpp" line="308"/>
+        <location filename="../browserwindow.cpp" line="315"/>
         <source>About SMTube</source>
         <translation>Acerca de SMTube</translation>
     </message>
     <message>
-        <location filename="../browserwindow.cpp" line="309"/>
+        <location filename="../browserwindow.cpp" line="316"/>
         <source>This is an early version of the new SMTube.</source>
         <translation>Esta es una versión preliminar del nuevo SMTube.</translation>
     </message>
     <message>
-        <location filename="../browserwindow.cpp" line="310"/>
+        <location filename="../browserwindow.cpp" line="317"/>
         <source>Many things are still missing.</source>
         <translation>Todavía faltan muchas cosas.</translation>
     </message>
     <message>
-        <location filename="../browserwindow.cpp" line="313"/>
+        <location filename="../browserwindow.cpp" line="320"/>
         <source>License: %1</source>
         <translation>Licencia: %1</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="322"/>
+        <source>Version: %1</source>
+        <translation>Versión: %1</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigDialog</name>
+    <message>
+        <location filename="../configdialog.ui" line="14"/>
+        <source>Settings</source>
+        <translation>Configuración</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="24"/>
+        <source>&amp;General</source>
+        <translation>&amp;General</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="32"/>
+        <source>Playback &amp;quality:</source>
+        <translation>Calidad de re&amp;producción:</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="74"/>
+        <source>&amp;Players</source>
+        <translation>&amp;Reproductores</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="85"/>
+        <source>Add</source>
+        <translation>Añadir</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="105"/>
+        <source>Up</source>
+        <translation>Subir</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="112"/>
+        <source>Delete</source>
+        <translation>Borrar</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="119"/>
+        <source>Down</source>
+        <translation>Bajar</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.cpp" line="104"/>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.cpp" line="104"/>
+        <source>Binary</source>
+        <translation>Ejecutable</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.cpp" line="104"/>
+        <source>Parameters</source>
+        <translation>Parámetros</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.cpp" line="104"/>
+        <source>Support for</source>
+        <translation>Soporte para</translation>
+    </message>
+</context>
+<context>
+    <name>FileChooser</name>
+    <message>
+        <location filename="../filechooser.cpp" line="55"/>
+        <source>Click to select a file or folder</source>
+        <translation>Pulsa para seleccionar un archivo o carpeta</translation>
     </message>
 </context>
 <context>
@@ -91,6 +172,19 @@
         <location filename="../mywebview.cpp" line="96"/>
         <source>Copy text to clipboard</source>
         <translation>Copiar texto al portapapeles</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../players.cpp" line="101"/>
+        <source>Video sites</source>
+        <translation>Webs de vídeos</translation>
+    </message>
+    <message>
+        <location filename="../players.cpp" line="101"/>
+        <source>Normal streams</source>
+        <translation>Datos normales</translation>
     </message>
 </context>
 </TS>
