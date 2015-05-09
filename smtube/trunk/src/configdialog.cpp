4fd9f88fc926abe9d5f987cb6261e8c9e0f092dd
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2015-05-09 23:59:18 +0000

    Use playerdialog to edit the players

--- smtube/trunk/src/configdialog.cpp
+++ smtube/trunk/src/configdialog.cpp
@@ -29,60 +29,6 @@
 #define COL_PARMS 2
 #define COL_DIRECTPLAY 3
 
-#ifdef USE_PLAYERS
-class TDelegate : public QItemDelegate
-{
-public:
-	TDelegate(QObject *parent = 0);
-
-	QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option,
-                           const QModelIndex &index) const;
-	virtual void setModelData(QWidget * editor, QAbstractItemModel * model,
-                              const QModelIndex & index ) const;
-};
-
-TDelegate::TDelegate(QObject *parent) : QItemDelegate(parent) {
-}
-
-QWidget * TDelegate::createEditor(QWidget *parent, const QStyleOptionViewItem & option, const QModelIndex & index) const {
-	//qDebug("TDelegate::createEditor");
-
-	if (index.column() == COL_BINARY) {
-		FileChooser * fch = new FileChooser(parent);
-		fch->setOptions(QFileDialog::DontUseNativeDialog | QFileDialog::DontResolveSymlinks); // Crashes if use the KDE dialog
-		fch->setText( index.model()->data(index, Qt::DisplayRole).toString() );
-		return fch;
-	}
-	if (index.column() == COL_DIRECTPLAY) {
-		QComboBox * combo = new QComboBox(parent);
-		combo->addItem(Player::directPlayToString(false), false);
-		combo->addItem(Player::directPlayToString(true), true);
-		combo->setCurrentIndex( index.model()->data(index, Qt::UserRole).toBool() ? 1 : 0 );
-		return combo;
-	}
-	else {
-		return QItemDelegate::createEditor(parent, option, index);
-	}
-}
-
-void TDelegate::setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const {
-	if (index.column() == COL_BINARY) {
-		FileChooser * fch = static_cast<FileChooser*>(editor);
-		model->setData(index, fch->text() );
-	}
-
-	if (index.column() == COL_DIRECTPLAY) {
-		QComboBox * combo = static_cast<QComboBox*>(editor);
-		int i = combo->currentIndex();
-		if (i > -1) {
-			bool b = combo->itemData(i).toBool();
-			model->setData(index, b, Qt::UserRole);
-			model->setData(index, Player::directPlayToString(b), Qt::DisplayRole);
-		}
-	}
-}
-#endif
-
 ConfigDialog::ConfigDialog(QWidget * parent, Qt::WindowFlags f)
     : QDialog(parent, f) 
 {
@@ -101,22 +47,20 @@ ConfigDialog::ConfigDialog(QWidget * parent, Qt::WindowFlags f)
 
 #ifdef USE_PLAYERS
 	// Setup player's table
-	table->setColumnCount(4);
-	table->setHorizontalHeaderLabels(QStringList() << tr("Name") << tr("Binary") << tr("Parameters") << tr("Support for") );
+	table->setColumnCount(1);
+	//table->setHorizontalHeaderLabels(QStringList() << tr("Name") << tr("Binary") << tr("Parameters") << tr("Support for") );
+	table->horizontalHeader()->setVisible(false);
 	table->verticalHeader()->setVisible(false);
 	table->setAlternatingRowColors(true);
 
 	#if QT_VERSION >= 0x050000
-	table->horizontalHeader()->setSectionResizeMode(COL_PARMS, QHeaderView::Stretch);
+	table->horizontalHeader()->setSectionResizeMode(COL_NAME, QHeaderView::Stretch);
 	#else
-	table->horizontalHeader()->setResizeMode(COL_PARMS, QHeaderView::Stretch);
+	table->horizontalHeader()->setResizeMode(COL_NAME, QHeaderView::Stretch);
 	#endif
 
 	table->setSelectionBehavior(QAbstractItemView::SelectRows);
 	table->setSelectionMode(QAbstractItemView::ExtendedSelection);
-
-	table->setItemDelegateForColumn( COL_BINARY, new TDelegate(table) );
-	table->setItemDelegateForColumn( COL_DIRECTPLAY, new TDelegate(table) );
 #endif
 }
 
@@ -144,22 +88,9 @@ void ConfigDialog::setPlayers(QList<Player> list) {
 		name_item->setData(Qt::UserRole + COL_BINARY, list[n].binary());
 		name_item->setData(Qt::UserRole + COL_PARMS, list[n].arguments());
 		name_item->setData(Qt::UserRole + COL_DIRECTPLAY, list[n].directPlay());
-
-		QTableWidgetItem * binary_item = new QTableWidgetItem;
-		binary_item->setText( list[n].binary() );
-
-		QTableWidgetItem * params_item = new QTableWidgetItem;
-		params_item->setText( list[n].arguments() );
-
-		QTableWidgetItem * dplay_item = new QTableWidgetItem;
-		bool value = list[n].directPlay();
-		dplay_item->setData(Qt::UserRole, value);
-		dplay_item->setData(Qt::DisplayRole, Player::directPlayToString(value));
+		name_item->setFlags(Qt::ItemIsEnabled | Qt::ItemIsSelectable);
 
 		table->setItem(n, COL_NAME, name_item);
-		table->setItem(n, COL_BINARY, binary_item);
-		table->setItem(n, COL_PARMS, params_item);
-		table->setItem(n, COL_DIRECTPLAY, dplay_item);
 	}
 
 	table->setCurrentCell(0, 0);
@@ -169,10 +100,11 @@ QList<Player> ConfigDialog::players() {
 	QList<Player> list;
 
 	for (int n = 0; n < table->rowCount(); n++) {
-		QString name = table->item(n, COL_NAME)->text();
-		QString binary = table->item(n, COL_BINARY)->text();
-		QString params = table->item(n, COL_PARMS)->text();
-		bool direct_play = table->item(n, COL_DIRECTPLAY)->data(Qt::UserRole).toBool();
+		QTableWidgetItem * i = table->item(n, COL_NAME);
+		QString name = i->data(Qt::UserRole + COL_NAME).toString();
+		QString binary = i->data(Qt::UserRole + COL_BINARY).toString();
+		QString params = i->data(Qt::UserRole + COL_PARMS).toString();
+		bool direct_play = i->data(Qt::UserRole + COL_DIRECTPLAY).toBool();
 
 		if (!name.isEmpty() && !binary.isEmpty()) {
 			list << Player(name, binary, params, direct_play);
@@ -224,16 +156,19 @@ void ConfigDialog::on_add_button_clicked() {
 	row++;
 	table->insertRow(row);
 
-	QTableWidgetItem * dplay_item = new QTableWidgetItem;
-	dplay_item->setData(Qt::UserRole, false);
-	dplay_item->setData(Qt::DisplayRole, Player::directPlayToString(false));
+	QTableWidgetItem * name_item = new QTableWidgetItem;
+	name_item->setText("");
+	name_item->setData(Qt::UserRole + COL_NAME, "");
+	name_item->setData(Qt::UserRole + COL_BINARY, "");
+	name_item->setData(Qt::UserRole + COL_PARMS, "%u");
+	name_item->setData(Qt::UserRole + COL_DIRECTPLAY, false);
+	name_item->setFlags(Qt::ItemIsEnabled | Qt::ItemIsSelectable);
 
-	table->setItem(row, COL_NAME, new QTableWidgetItem);
-	table->setItem(row, COL_BINARY, new QTableWidgetItem);
-	table->setItem(row, COL_PARMS, new QTableWidgetItem);
-	table->setItem(row, COL_DIRECTPLAY, dplay_item);
+	table->setItem(row, COL_NAME, name_item);
 
 	table->setCurrentCell(row, table->currentColumn());
+
+	on_edit_button_clicked();
 }
 
 void ConfigDialog::on_up_button_clicked() {
