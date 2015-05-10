24478c02bc8b980f8cb65421ad39bedd0ee9e94a
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2015-05-10 22:12:27 +0000

    Change version to 15.5.10

--- smtube/trunk/src/version.cpp
+++ smtube/trunk/src/version.cpp
@@ -18,9 +18,9 @@
 
 #include "version.h"
 
-#define USE_SVN_VERSIONS 1
+#define USE_SVN_VERSIONS 0
 
-#define VERSION "15.5.7"
+#define VERSION "15.5.10"
 
 #if USE_SVN_VERSIONS
 #include "svn_revision.h"
