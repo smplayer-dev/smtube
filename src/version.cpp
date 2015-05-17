d91e2a9a5eac49698b1ac1b0c68edb9d35dd5720
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2015-05-17 22:49:22 +0000

    Change version to 15.5.17

--- src/version.cpp
+++ src/version.cpp
@@ -18,9 +18,9 @@
 
 #include "version.h"
 
-#define USE_SVN_VERSIONS 1
+#define USE_SVN_VERSIONS 0
 
-#define VERSION "15.5.10"
+#define VERSION "15.5.17"
 
 #if USE_SVN_VERSIONS
 #include "svn_revision.h"
