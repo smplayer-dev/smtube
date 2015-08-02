0dcdca3634fdad2ca86aa98e9043aad44e1df6b6
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2015-08-02 22:10:35 +0000

    Update translations

--- smtube/trunk/src/translations/smtube_de.ts
+++ smtube/trunk/src/translations/smtube_de.ts
@@ -1,6 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="de">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="de" version="2.0">
 <context>
     <name>About</name>
     <message>
@@ -413,12 +411,12 @@
     <message>
         <location filename="../playerdialog.ui" line="80"/>
         <source>Preferred &amp;quality:</source>
-        <translation type="unfinished">Bevorzugte &amp;Qualität:</translation>
+        <translation>Bevorzugte &amp;Qualität:</translation>
     </message>
     <message>
         <location filename="../playerdialog.ui" line="110"/>
         <source>Add to &amp;menu:</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu &amp;Menü hinzufügen:</translation>
     </message>
     <message>
         <location filename="../playerdialog.cpp" line="28"/>
@@ -443,7 +441,7 @@
     <message>
         <location filename="../playerdialog.cpp" line="33"/>
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Standard</translation>
     </message>
 </context>
 <context>
