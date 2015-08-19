db6c5e05b5805de8231a881eb82374194792b327
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2015-08-19 22:18:22 +0000

    Add the Turkish  translation

---
+++ src/translations/smtube_tr.ts
@@ -0,0 +1,460 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="tr" version="2.0">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../about.ui" line="14"/>
+        <source>About...</source>
+        <translation>Hakkında...</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="33"/>
+        <source>&amp;Info</source>
+        <translation>&amp;Bilgi</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="89"/>
+        <source>&amp;Translators</source>
+        <translation>&amp;Çevirenler</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="145"/>
+        <source>&amp;License</source>
+        <translation>&amp;Lisans</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="33"/>
+        <source>YouTube%1 Browser for SMPlayer</source>
+        <translation>YouTube%1 Tarayıcı SMPlayer</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="36"/>
+        <source>Version: %1</source>
+        <translation>Versiyon: %1</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="38"/>
+        <source>Portable Edition</source>
+        <translation>Katılımsız Sürüm</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="41"/>
+        <source>Compiled with Qt %1 (using %2)</source>
+        <translation>Qt %1 ile derlendi (%2 kullanılarak)</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="43"/>
+        <source>Visit our website for updates:</source>
+        <translation>Güncellemeler için web adresini ziyaret edin:</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="86"/>
+        <source>Many people contributed with translations.</source>
+        <translation>Birçok insan çevirilere katkıda bulundu.</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="87"/>
+        <source>You can also help to translate SMTube into your own language.</source>
+        <translation>Ayrıca SMPlayerı kendi dilinize çevirmek için yadım edebilirsiniz.</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="88"/>
+        <source>Visit %1 and join a translation team.</source>
+        <translation>%1 sayfasını ziyaret edip çeviri takımına katılabilirsiniz.</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="90"/>
+        <source>Click here to know the translators from the transifex teams</source>
+        <translation>Transifex takımındaki çevirmenleri görmek için tıkla</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="116"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; (%2)</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; (%2)</translation>
+    </message>
+</context>
+<context>
+    <name>BrowserWindow</name>
+    <message>
+        <location filename="../browserwindow.cpp" line="116"/>
+        <source>Home</source>
+        <translation>Anasayfa</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="119"/>
+        <source>Navigation</source>
+        <translation>Gezinti</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="127"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Kapat</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="130"/>
+        <source>&amp;Navigation</source>
+        <translation>&amp;Gezinti</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="139"/>
+        <source>&amp;View</source>
+        <translation>&amp;Gösterim</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="140"/>
+        <source>Toolbar</source>
+        <translation>Araç kutusu</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="144"/>
+        <source>Statusbar</source>
+        <translation>Durum çubuğu</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="148"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Ayarlar</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="156"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Yardım</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="159"/>
+        <source>&amp;Update the YouTube code</source>
+        <translation>&amp;Youtube kodlarını güncelle</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="164"/>
+        <source>About this &amp;release</source>
+        <translation>Bu &amp;sürüm hakkında</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="168"/>
+        <source>&amp;About SMTube</source>
+        <translation>&amp;SMPlayer Hakkında</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="418"/>
+        <source>Connection failed</source>
+        <translation>Bağlantı başarısız oldu</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="419"/>
+        <source>The video you requested needs to open a HTTPS connection.</source>
+        <translation>İstenen video HTTPS bağlantı açması gerekiyor.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="420"/>
+        <source>Unfortunately the OpenSSL component, required for it, is not available in your system.</source>
+        <translation>Ne yazık ki openssl bileşeni gerekli bunun için sisteminizde mevcut değil.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="425"/>
+        <source>No video found</source>
+        <translation>Video bulunamadı</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="426"/>
+        <source>It wasn&apos;t possible to find the URL for this video.</source>
+        <translation>Video bağlantısı bulunamadı</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="436"/>
+        <location filename="../browserwindow.cpp" line="444"/>
+        <source>Problems with YouTube</source>
+        <translation>YouTube problemi</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="437"/>
+        <location filename="../browserwindow.cpp" line="445"/>
+        <source>Unfortunately due to changes in YouTube, the video &apos;%1&apos; can&apos;t be played.</source>
+        <translation>Ne yazık ki YouTube sayfasındaki değişiklik nedeniyle, video &apos;%1&apos; oynatılamadı.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="438"/>
+        <source>Do you want to update the YouTube code? This may fix the problem.</source>
+        <translation>YouTube kodlarını güncellemek istiyor musun? Bu bazı problemleri çözecek.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="446"/>
+        <source>Maybe updating this application could fix the problem.</source>
+        <translation>Belki uygulamayı güncellemek sorunu çözebilir.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="464"/>
+        <source>About this release</source>
+        <translation>Bu sürüm hakkında</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="465"/>
+        <source>Due to changes in YouTube, the old SMTube doesn&apos;t work anymore.</source>
+        <translation>Ne yazık ki YouTube sayfasındaki değişiklik nedeniyle, eski SMTube doğru çalışamıyor.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="466"/>
+        <source>This is a new version of SMTube, written from scratch.</source>
+        <translation>Bu yeni SMTube sürümü sıfırdan yazılmıştır.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="467"/>
+        <source>Some functionality is not available yet.</source>
+        <translation>Bazı fonksiyonlar mevcut değil.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="469"/>
+        <source>Important:</source>
+        <translation>Önemli:</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="470"/>
+        <source>If the VEVO videos fail to play, please use the option %1 in the Help menu.</source>
+        <translation>Eğer VEVO videolarını oynatamıyorsanız, %1 seçeneğini Yardım menüsünden bulun.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="471"/>
+        <source>Update the YouTube code</source>
+        <translation>YouTube kodlarını güncelle</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="239"/>
+        <source>Loading... %1%</source>
+        <translation>Yükleniyor.. %1%</translation>
+    </message>
+</context>
+<context>
+    <name>CodeDownloader</name>
+    <message>
+        <location filename="../codedownloader.cpp" line="37"/>
+        <source>Downloading...</source>
+        <translation>İndiriyor...</translation>
+    </message>
+    <message>
+        <location filename="../codedownloader.cpp" line="58"/>
+        <source>Connecting to %1</source>
+        <translation>%1&apos;e bağlanıyor</translation>
+    </message>
+    <message>
+        <location filename="../codedownloader.cpp" line="126"/>
+        <source>The Youtube code has been updated successfully.</source>
+        <translation>YouTube kodlarını başarıyla güncellendi.</translation>
+    </message>
+    <message>
+        <location filename="../codedownloader.cpp" line="127"/>
+        <source>Installed version: %1</source>
+        <translation>Kurulu sürüm: %1</translation>
+    </message>
+    <message>
+        <location filename="../codedownloader.cpp" line="128"/>
+        <source>Success</source>
+        <translation>Başarılı</translation>
+    </message>
+    <message>
+        <location filename="../codedownloader.cpp" line="133"/>
+        <location filename="../codedownloader.cpp" line="138"/>
+        <source>Error</source>
+        <translation>Hata</translation>
+    </message>
+    <message>
+        <location filename="../codedownloader.cpp" line="133"/>
+        <source>An error happened writing %1</source>
+        <translation>Bir yazma hatası meydana geldi %1</translation>
+    </message>
+    <message>
+        <location filename="../codedownloader.cpp" line="138"/>
+        <source>An error happened while downloading the file:&lt;br&gt;%1</source>
+        <translation>Dosya indirilirken bir hata meydana geldi:&lt;br&gt;%1</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigDialog</name>
+    <message>
+        <location filename="../configdialog.ui" line="14"/>
+        <source>Settings</source>
+        <translation>Ayarlar</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="24"/>
+        <source>&amp;General</source>
+        <translation>&amp;Genel</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="113"/>
+        <source>Add</source>
+        <translation>Ekle</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="78"/>
+        <source>Up</source>
+        <translation>Yukarı</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="30"/>
+        <source>Playback</source>
+        <translation>Oynatma</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="36"/>
+        <source>Preferred &amp;quality:</source>
+        <translation>Tercih edilen &amp;kalite:</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="65"/>
+        <source>Players</source>
+        <translation>Oynatıcılar</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="124"/>
+        <source>Delete</source>
+        <translation>Sil</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="148"/>
+        <source>Edit</source>
+        <translation>Düzenle</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="161"/>
+        <source>A&amp;dd predefined players</source>
+        <translation>&amp;Önceden ayarlı oynatıcı ekle</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="89"/>
+        <source>Down</source>
+        <translation>Aşağı</translation>
+    </message>
+</context>
+<context>
+    <name>FileChooser</name>
+    <message>
+        <location filename="../filechooser.cpp" line="56"/>
+        <source>Click to select a file or folder</source>
+        <translation>Dosya veya klasör seçmek için tıklayın</translation>
+    </message>
+</context>
+<context>
+    <name>MyWebView</name>
+    <message>
+        <location filename="../mywebview.cpp" line="36"/>
+        <source>Open link in a web browser</source>
+        <translation>Web tarayıcıda linki aç</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="52"/>
+        <source>Open link in new window</source>
+        <translation>Yeni pencerede linki aç</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="75"/>
+        <source>Open audio with</source>
+        <translation>Ses dosyasını aç</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="91"/>
+        <location filename="../mywebview.cpp" line="116"/>
+        <source>Open with %1</source>
+        <translation>&amp;1 ile Aç</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="123"/>
+        <source>Open audio with %1</source>
+        <translation>&amp;1 ile ses dosyasını aç</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="49"/>
+        <source>Copy link to clipboard</source>
+        <translation>Bağlantıyı panoya kopyala</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="48"/>
+        <source>Save link...</source>
+        <translation>Linki kaydet...</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="50"/>
+        <source>Open link in this window</source>
+        <translation>Bu pencerede linki aç</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="51"/>
+        <source>Copy text to clipboard</source>
+        <translation>Yazıyı panoya kopyala</translation>
+    </message>
+</context>
+<context>
+    <name>PlayerDialog</name>
+    <message>
+        <location filename="../playerdialog.ui" line="14"/>
+        <source>Edit Player</source>
+        <translation>Oynatıcıyı düzenle</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="22"/>
+        <source>&amp;Name:</source>
+        <translation>&amp;İsim:</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="39"/>
+        <source>&amp;Executable:</source>
+        <translation>%Çalıştırılabilir:</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="56"/>
+        <source>&amp;Parameters:</source>
+        <translation>&amp;Parametre:</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="71"/>
+        <source>This player supports &amp;video sites</source>
+        <translation>Bu oynatıcı video sitelerini destekler</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="80"/>
+        <source>Preferred &amp;quality:</source>
+        <translation>Tercih edilen &amp;kalite:</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="110"/>
+        <source>Add to &amp;menu:</source>
+        <translation>&amp;Menüye ekle</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.cpp" line="28"/>
+        <source>None</source>
+        <translation>Hiçbiri</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.cpp" line="29"/>
+        <source>Video</source>
+        <translation>Video</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.cpp" line="30"/>
+        <source>Audio</source>
+        <translation>Ses</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.cpp" line="31"/>
+        <source>Video and audio</source>
+        <translation>Video ve ses</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.cpp" line="33"/>
+        <source>Default</source>
+        <translation>Varsayılan</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../players.cpp" line="101"/>
+        <source>Video sites</source>
+        <translation>Video siteleri</translation>
+    </message>
+    <message>
+        <location filename="../players.cpp" line="101"/>
+        <source>Normal streams</source>
+        <translation>Normal akış</translation>
+    </message>
+</context>
+</TS>
