90d058d5263dca7675c05d8bd6d344cbd74e00a1
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2015-08-19 22:20:59 +0000

    Add the Hungarian translation

---
+++ src/translations/smtube_hu.ts
@@ -0,0 +1,460 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="hu" version="2.0">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../about.ui" line="14"/>
+        <source>About...</source>
+        <translation>Névjegy...</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="33"/>
+        <source>&amp;Info</source>
+        <translation>&amp;Infó</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="89"/>
+        <source>&amp;Translators</source>
+        <translation>&amp;Fordítók</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="145"/>
+        <source>&amp;License</source>
+        <translation>&amp;Licensz</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="33"/>
+        <source>YouTube%1 Browser for SMPlayer</source>
+        <translation>YouTube%1 böngésző - SMPlayer</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="36"/>
+        <source>Version: %1</source>
+        <translation>Verzió: %1</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="38"/>
+        <source>Portable Edition</source>
+        <translation>Hordozható változat</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="41"/>
+        <source>Compiled with Qt %1 (using %2)</source>
+        <translation>Fordítva Qt %1-l (használt Qt: %2)</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="43"/>
+        <source>Visit our website for updates:</source>
+        <translation>Frissítésekért látogassa meg a weboldalunkat:</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="86"/>
+        <source>Many people contributed with translations.</source>
+        <translation>Sokan hozzájárultak a fordításhoz.</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="87"/>
+        <source>You can also help to translate SMTube into your own language.</source>
+        <translation>Ön is segíthet lefordítani az SMPlayer-t a saját nyelvére.</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="88"/>
+        <source>Visit %1 and join a translation team.</source>
+        <translation>Látogassa meg a %1 címet és csatlakozzon valamelyik fordítói csapathoz.</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="90"/>
+        <source>Click here to know the translators from the transifex teams</source>
+        <translation>Kattintson ide, hogy többet megtudjon a Transifex csapatok jelenlegi fordítóiról</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="116"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; (%2)</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; (%2)</translation>
+    </message>
+</context>
+<context>
+    <name>BrowserWindow</name>
+    <message>
+        <location filename="../browserwindow.cpp" line="116"/>
+        <source>Home</source>
+        <translation>Kezdőlap</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="119"/>
+        <source>Navigation</source>
+        <translation>Navigáció</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="127"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Kilépés</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="130"/>
+        <source>&amp;Navigation</source>
+        <translation>&amp;Navigáció</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="139"/>
+        <source>&amp;View</source>
+        <translation>&amp;Nézet</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="140"/>
+        <source>Toolbar</source>
+        <translation>Eszköztár</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="144"/>
+        <source>Statusbar</source>
+        <translation>Állapotsor</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="148"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Beállítások</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="156"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Segítség</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="159"/>
+        <source>&amp;Update the YouTube code</source>
+        <translation>YouTube kód &amp;frissítése</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="164"/>
+        <source>About this &amp;release</source>
+        <translation>A jelenlegi &amp;kiadásról</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="168"/>
+        <source>&amp;About SMTube</source>
+        <translation>SMTube &amp;névjegye</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="418"/>
+        <source>Connection failed</source>
+        <translation>Sikertelen kapcsolat</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="419"/>
+        <source>The video you requested needs to open a HTTPS connection.</source>
+        <translation>Ön által kért videóhoz HTTPS kapcsolatot kell megnyitni.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="420"/>
+        <source>Unfortunately the OpenSSL component, required for it, is not available in your system.</source>
+        <translation>Sajnos, az Ön rendszerén nem állnak rendelkezésre a szükséges OpenSSL komponensek.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="425"/>
+        <source>No video found</source>
+        <translation>Nincs videótalálat</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="426"/>
+        <source>It wasn&apos;t possible to find the URL for this video.</source>
+        <translation>A videó nem található a megadott URL címen.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="436"/>
+        <location filename="../browserwindow.cpp" line="444"/>
+        <source>Problems with YouTube</source>
+        <translation>Problémák a Youtube-bal</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="437"/>
+        <location filename="../browserwindow.cpp" line="445"/>
+        <source>Unfortunately due to changes in YouTube, the video &apos;%1&apos; can&apos;t be played.</source>
+        <translation>Sajnos, a Youtube-on történt változások miatt, a(z) &apos;%1&apos; videót nem lehet lejátszani.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="438"/>
+        <source>Do you want to update the YouTube code? This may fix the problem.</source>
+        <translation>Szeretné frissíteni a Youtube kódot? Ez megoldhatja a problémát.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="446"/>
+        <source>Maybe updating this application could fix the problem.</source>
+        <translation>Lehet, hogy az alkalmazás frissítése megoldja a problémát.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="464"/>
+        <source>About this release</source>
+        <translation>A jelenlegi kiadásról</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="465"/>
+        <source>Due to changes in YouTube, the old SMTube doesn&apos;t work anymore.</source>
+        <translation>A YouTube-on történt változások miatt, a régi SMTube már nem működik. </translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="466"/>
+        <source>This is a new version of SMTube, written from scratch.</source>
+        <translation>Az SMTube ezen verziója a nulláról lett újraírva.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="467"/>
+        <source>Some functionality is not available yet.</source>
+        <translation>Ezért néhány funkció még nem elérhető.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="469"/>
+        <source>Important:</source>
+        <translation>Fontos:</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="470"/>
+        <source>If the VEVO videos fail to play, please use the option %1 in the Help menu.</source>
+        <translation>Ha a VEVO videókat nem tudja lejátszani, kérem használja a Segítség menüből a %1 menüpontot.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="471"/>
+        <source>Update the YouTube code</source>
+        <translation>YouTube kód frissítése</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="239"/>
+        <source>Loading... %1%</source>
+        <translation>Betöltve... %1%</translation>
+    </message>
+</context>
+<context>
+    <name>CodeDownloader</name>
+    <message>
+        <location filename="../codedownloader.cpp" line="37"/>
+        <source>Downloading...</source>
+        <translation>Letöltés...</translation>
+    </message>
+    <message>
+        <location filename="../codedownloader.cpp" line="58"/>
+        <source>Connecting to %1</source>
+        <translation>Kapcsolódás %1-hoz</translation>
+    </message>
+    <message>
+        <location filename="../codedownloader.cpp" line="126"/>
+        <source>The Youtube code has been updated successfully.</source>
+        <translation>A Youtube kód sikeresen frissítve.</translation>
+    </message>
+    <message>
+        <location filename="../codedownloader.cpp" line="127"/>
+        <source>Installed version: %1</source>
+        <translation>Telepített verzió: %1</translation>
+    </message>
+    <message>
+        <location filename="../codedownloader.cpp" line="128"/>
+        <source>Success</source>
+        <translation>Sikerült</translation>
+    </message>
+    <message>
+        <location filename="../codedownloader.cpp" line="133"/>
+        <location filename="../codedownloader.cpp" line="138"/>
+        <source>Error</source>
+        <translation>Hiba</translation>
+    </message>
+    <message>
+        <location filename="../codedownloader.cpp" line="133"/>
+        <source>An error happened writing %1</source>
+        <translation>Hiba történt írás közben: %1</translation>
+    </message>
+    <message>
+        <location filename="../codedownloader.cpp" line="138"/>
+        <source>An error happened while downloading the file:&lt;br&gt;%1</source>
+        <translation>Hiba történt a fájl letöltése közben:&lt;br&gt;%1</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigDialog</name>
+    <message>
+        <location filename="../configdialog.ui" line="14"/>
+        <source>Settings</source>
+        <translation>Beállítások</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="24"/>
+        <source>&amp;General</source>
+        <translation>&amp;Általános</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="113"/>
+        <source>Add</source>
+        <translation>Hozzáadás</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="78"/>
+        <source>Up</source>
+        <translation>Fel</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="30"/>
+        <source>Playback</source>
+        <translation>Lejátszás</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="36"/>
+        <source>Preferred &amp;quality:</source>
+        <translation>Előnyben részesített &amp;minőség:</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="65"/>
+        <source>Players</source>
+        <translation>Lejátszók</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="124"/>
+        <source>Delete</source>
+        <translation>Törlés</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="148"/>
+        <source>Edit</source>
+        <translation>Szerkesztés</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="161"/>
+        <source>A&amp;dd predefined players</source>
+        <translation>Előre meghatározott lejátszók &amp;hozzáadása</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="89"/>
+        <source>Down</source>
+        <translation>Le</translation>
+    </message>
+</context>
+<context>
+    <name>FileChooser</name>
+    <message>
+        <location filename="../filechooser.cpp" line="56"/>
+        <source>Click to select a file or folder</source>
+        <translation>Válasszon egy fájlt vagy egy mappát</translation>
+    </message>
+</context>
+<context>
+    <name>MyWebView</name>
+    <message>
+        <location filename="../mywebview.cpp" line="36"/>
+        <source>Open link in a web browser</source>
+        <translation>Hivatkozás megnyitása webböngészőben</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="52"/>
+        <source>Open link in new window</source>
+        <translation>Hivatkozás megnyitása új ablakban</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="75"/>
+        <source>Open audio with</source>
+        <translation>Hang megnyitása ezzel:</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="91"/>
+        <location filename="../mywebview.cpp" line="116"/>
+        <source>Open with %1</source>
+        <translation>Megnyitás ezzel: %1</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="123"/>
+        <source>Open audio with %1</source>
+        <translation>Hang megnyitása ezzel: %1</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="49"/>
+        <source>Copy link to clipboard</source>
+        <translation>Hivatkozás másolása vágólapra</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="48"/>
+        <source>Save link...</source>
+        <translation>Hivatkozás mentése...</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="50"/>
+        <source>Open link in this window</source>
+        <translation>Hivatkozás megnyitása ebben az ablakban</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="51"/>
+        <source>Copy text to clipboard</source>
+        <translation>Szöveg másolása vágólapra</translation>
+    </message>
+</context>
+<context>
+    <name>PlayerDialog</name>
+    <message>
+        <location filename="../playerdialog.ui" line="14"/>
+        <source>Edit Player</source>
+        <translation>Lejátszó szerkesztése</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="22"/>
+        <source>&amp;Name:</source>
+        <translation>&amp;Név:</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="39"/>
+        <source>&amp;Executable:</source>
+        <translation>&amp;Futtatható állomány:</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="56"/>
+        <source>&amp;Parameters:</source>
+        <translation>&amp;Paraméterek:</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="71"/>
+        <source>This player supports &amp;video sites</source>
+        <translation>A lejátszó támogatja a &amp;videó oldalakat</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="80"/>
+        <source>Preferred &amp;quality:</source>
+        <translation>Előnyben részesített &amp;minőség:</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="110"/>
+        <source>Add to &amp;menu:</source>
+        <translation>Hozzáadás a &amp;menühöz:</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.cpp" line="28"/>
+        <source>None</source>
+        <translation>Semmi</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.cpp" line="29"/>
+        <source>Video</source>
+        <translation>Videó</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.cpp" line="30"/>
+        <source>Audio</source>
+        <translation>Hang</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.cpp" line="31"/>
+        <source>Video and audio</source>
+        <translation>Videó és hang</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.cpp" line="33"/>
+        <source>Default</source>
+        <translation>Alapértelmezett</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../players.cpp" line="101"/>
+        <source>Video sites</source>
+        <translation>Videó oldalak</translation>
+    </message>
+    <message>
+        <location filename="../players.cpp" line="101"/>
+        <source>Normal streams</source>
+        <translation>Normál adatfolyamok</translation>
+    </message>
+</context>
+</TS>
