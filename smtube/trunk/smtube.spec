fd30b8ad5b61eb088b5454fef9a10e38a35eb4fb
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2016-03-03 01:28:01 +0000

    Change version to 16.3.0

--- smtube/trunk/smtube.spec
+++ smtube/trunk/smtube.spec
@@ -1,5 +1,5 @@
 Name:           smtube
-Version:        16.1.0
+Version:        16.3.0
 Release:        1%{?dist}
 Summary:        YouTube browser for SMPlayer
 
--- smtube/trunk/src/version.cpp
+++ smtube/trunk/src/version.cpp
@@ -18,9 +18,9 @@
 
 #include "version.h"
 
-#define USE_SVN_VERSIONS 1
+#define USE_SVN_VERSIONS 0
 
-#define VERSION "16.1.0"
+#define VERSION "16.3.0"
 
 #if USE_SVN_VERSIONS
 #include "svn_revision.h"
