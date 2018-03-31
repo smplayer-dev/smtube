bdf68d789cb777446f17ef626a20b943f65ef824
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2018-03-31 23:06:12 +0000

    Change version to 18.3.0

--- smtube/trunk/src/version.cpp
+++ smtube/trunk/src/version.cpp
@@ -18,9 +18,9 @@
 
 #include "version.h"
 
-#define USE_SVN_VERSIONS 1
+#define USE_SVN_VERSIONS 0
 
-#define VERSION "18.1.0"
+#define VERSION "18.3.0"
 
 #if USE_SVN_VERSIONS
 #include "svn_revision.h"
