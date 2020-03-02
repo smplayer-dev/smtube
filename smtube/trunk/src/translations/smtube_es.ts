bfca7cf478c6dd607ac1c353637a3682708a7fb7
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2020-03-02 21:26:37 +0000

    Add smtube_es.ts

---
+++ smtube/trunk/src/translations/smtube_es.ts
@@ -0,0 +1,472 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es" version="2.0">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../about.ui" line="14"/>
+        <source>About...</source>
+        <translation>Acerca de...</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="33"/>
+        <source>&amp;Info</source>
+        <translation>&amp;Información</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="89"/>
+        <source>&amp;Translators</source>
+        <translation>&amp;Traductores</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="145"/>
+        <source>&amp;License</source>
+        <translation>&amp;Licencia</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="34"/>
+        <source>YouTube%1 Browser for SMPlayer</source>
+        <translation>Navegador de YouTube%1 de SMPlayer</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="37"/>
+        <source>Version: %1</source>
+        <translation>Versión: %1</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="39"/>
+        <source>Portable Edition</source>
+        <translation>Edición portable</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="42"/>
+        <source>Compiled with Qt %1 (using %2)</source>
+        <translation>Compilado con Qt %1 (usando %2)</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="44"/>
+        <source>Visit our website for updates:</source>
+        <translation>Visite nuestra web para conseguir actualizaciones:</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="87"/>
+        <source>Many people contributed with translations.</source>
+        <translation>Muchas personas han contribuido con traducciones.</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="88"/>
+        <source>You can also help to translate SMTube into your own language.</source>
+        <translation>También puede ayudar a traducir SMTube a su propio idioma.</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="89"/>
+        <source>Visit %1 and join a translation team.</source>
+        <translation>Visite %1 y únase a un equipo de traducción.</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="91"/>
+        <source>Click here to know the translators from the transifex teams</source>
+        <translation>Haga clic aquí para conocer a los traductores de los equipos de Transifex</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="117"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; (%2)</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; (%2)</translation>
+    </message>
+</context>
+<context>
+    <name>BrowserWindow</name>
+    <message>
+        <location filename="../browserwindow.cpp" line="125"/>
+        <source>Home</source>
+        <translation>Inicio</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="128"/>
+        <source>Navigation</source>
+        <translation>Navegación</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="136"/>
+        <source>Zoom +</source>
+        <translation>Ampliar</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="140"/>
+        <source>Zoom -</source>
+        <translation>Reducir</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="144"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Salir</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="147"/>
+        <source>&amp;Navigation</source>
+        <translation>&amp;Navegación</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="159"/>
+        <source>&amp;View</source>
+        <translation>&amp;Ver</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="160"/>
+        <source>Toolbar</source>
+        <translation>Barra de herramientas</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="164"/>
+        <source>Statusbar</source>
+        <translation>Barra de estado</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="168"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Configuración</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="176"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Ayuda</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="179"/>
+        <source>&amp;Update the YouTube code</source>
+        <translation>Actualizar el código de &amp;YouTube</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="185"/>
+        <source>About this &amp;release</source>
+        <translation>Acerca de esta &amp;version</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="190"/>
+        <source>&amp;About SMTube</source>
+        <translation>&amp;Acerca de SMTube</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="298"/>
+        <source>Download video using an external service</source>
+        <translation>Descargar el vídeo usando un servicio externo</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="299"/>
+        <source>Download</source>
+        <translation>Descargar</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="527"/>
+        <source>Connection failed</source>
+        <translation>Fallo en la conexión</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="528"/>
+        <source>The video you requested needs to open a HTTPS connection.</source>
+        <translation>El vídeo que ha solicitado necesita abrir una conexión HTTPS.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="529"/>
+        <source>Unfortunately the OpenSSL component, required for it, is not available in your system.</source>
+        <translation>Lamentablemente el componente OpenSSL, necesario para ello, no no está disponible en el sistema.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="534"/>
+        <source>No video found</source>
+        <translation>No se ha encontrado ningún vídeo</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="535"/>
+        <source>It wasn&apos;t possible to find the URL for this video.</source>
+        <translation>No ha sido posible encontrar el URL de este vídeo.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="545"/>
+        <location filename="../browserwindow.cpp" line="553"/>
+        <source>Problems with YouTube</source>
+        <translation>Problemas con YouTube</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="546"/>
+        <location filename="../browserwindow.cpp" line="554"/>
+        <source>Unfortunately due to changes in YouTube, the video &apos;%1&apos; can&apos;t be played.</source>
+        <translation>Lamentablemente debido a cambios en YouTube, no se puede reproducir el vídeo «%1».</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="547"/>
+        <source>Do you want to update the YouTube code? This may fix the problem.</source>
+        <translation>¿Quiere actualizar el código de YouTube? Esto podría solucionar el problema.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="555"/>
+        <source>Maybe updating this application could fix the problem.</source>
+        <translation>Quizás el problema se podría solucionar actualizando esta aplicación.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="574"/>
+        <source>About this release</source>
+        <translation>Acerca de esta version</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="575"/>
+        <source>Due to changes in YouTube, the old SMTube doesn&apos;t work anymore.</source>
+        <translation>Debido a cambios en YouTube, la versión anterior de SMTube ha dejado de funcionar.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="576"/>
+        <source>This is a new version of SMTube, written from scratch.</source>
+        <translation>Esta versión de SMTube es completamente nueva, escrita desde cero.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="577"/>
+        <source>Some functionality is not available yet.</source>
+        <translation>Algunas funciones no están disponibles todavía.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="579"/>
+        <source>Important:</source>
+        <translation>Importante:</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="580"/>
+        <source>If the VEVO videos fail to play, please use the option %1 in the Help menu.</source>
+        <translation>Si los vídeos de VEVO no se reproducen, use la opción %1 del menú Ayuda.</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="581"/>
+        <source>Update the YouTube code</source>
+        <translation>Actualizar el código de YouTube</translation>
+    </message>
+    <message>
+        <location filename="../browserwindow.cpp" line="284"/>
+        <source>Loading... %1%</source>
+        <translation>Cargando... %1%</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigDialog</name>
+    <message>
+        <location filename="../configdialog.ui" line="14"/>
+        <source>Settings</source>
+        <translation>Configuración</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="27"/>
+        <source>&amp;General</source>
+        <translation>&amp;General</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="108"/>
+        <source>&amp;Players</source>
+        <translation>&amp;Reproductores</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="156"/>
+        <source>Add</source>
+        <translation>Añadir</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="121"/>
+        <source>Up</source>
+        <translation>Subir</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="33"/>
+        <source>Playback</source>
+        <translation>Reproducción</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="39"/>
+        <source>Preferred &amp;quality:</source>
+        <translation>Calidad &amp;preferida:</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="167"/>
+        <source>Delete</source>
+        <translation>Eliminar</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="191"/>
+        <source>Edit</source>
+        <translation>Editar</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="204"/>
+        <source>A&amp;dd predefined players</source>
+        <translation>Añadir los &amp;reproductores predeterminados</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="240"/>
+        <source>&amp;Interface</source>
+        <translation>&amp;Interfaz</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="248"/>
+        <source>&amp;Style:</source>
+        <translation>&amp;Estilo:</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="284"/>
+        <source>Font:</source>
+        <translation>Tipo de letra:</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="298"/>
+        <source>C&amp;hange...</source>
+        <translation>Ca&amp;mbiar...</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="68"/>
+        <source>Download</source>
+        <translation>Descargas</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="74"/>
+        <source>Download videos using an &amp;external service</source>
+        <translation>Descargar vídeos usando un servicio &amp;externo</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="132"/>
+        <source>Down</source>
+        <translation>Bajar</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.cpp" line="74"/>
+        <source>Default</source>
+        <translation>Predeterminado</translation>
+    </message>
+</context>
+<context>
+    <name>FileChooser</name>
+    <message>
+        <location filename="../filechooser.cpp" line="56"/>
+        <source>Click to select a file or folder</source>
+        <translation>Haga clic para seleccionar un archivo o una carpeta</translation>
+    </message>
+</context>
+<context>
+    <name>MyWebView</name>
+    <message>
+        <location filename="../mywebview.cpp" line="37"/>
+        <source>Open in &amp;YouTube</source>
+        <translation>Abrir en &amp;YouTube</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="48"/>
+        <source>&amp;Save link...</source>
+        <translation>&amp;Descargar el enlace...</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="51"/>
+        <source>Copy &amp;link to clipboard</source>
+        <translation>Copiar el en&amp;lace al portapapeles</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="52"/>
+        <source>Open link in this window</source>
+        <translation>Abrir el enlace en esta ventana</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="54"/>
+        <source>&amp;Copy text to clipboard</source>
+        <translation>&amp;Copiar el texto al portapapeles</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="55"/>
+        <source>Open link in &amp;new window</source>
+        <translation>Abrir el enlace en una &amp;nueva ventana</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="79"/>
+        <source>&amp;Play with</source>
+        <translation>&amp;Reproducir con</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="84"/>
+        <source>Play &amp;audio with</source>
+        <translation>Reproducir el &amp;audio con</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="137"/>
+        <source>&amp;Play with %1</source>
+        <translation>&amp;Reproducir con %1</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="144"/>
+        <source>Play &amp;audio with %1</source>
+        <translation>Reproducir el &amp;audio con %1</translation>
+    </message>
+    <message>
+        <location filename="../mywebview.cpp" line="151"/>
+        <source>Play video with a &amp;web browser</source>
+        <translation>Reproducir el vídeo en un navegador &amp;web</translation>
+    </message>
+</context>
+<context>
+    <name>PlayerDialog</name>
+    <message>
+        <location filename="../playerdialog.ui" line="14"/>
+        <source>Edit Player</source>
+        <translation>Editar el reproductor</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="22"/>
+        <source>&amp;Name:</source>
+        <translation>&amp;Nombre:</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="39"/>
+        <source>&amp;Executable:</source>
+        <translation>&amp;Ejecutable:</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="56"/>
+        <source>&amp;Parameters:</source>
+        <translation>&amp;Parámetros:</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="71"/>
+        <source>This player supports &amp;video sites</source>
+        <translation>Este reproductor admite sitios web de &amp;vídeos</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="78"/>
+        <source>This player can play &amp;online TV</source>
+        <translation>Este reproductor puede repr&amp;oducir TV en línea</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="87"/>
+        <source>Preferred &amp;quality:</source>
+        <translation>Calidad pre&amp;ferida:</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.ui" line="117"/>
+        <source>Add to &amp;menu:</source>
+        <translation>Añadir al &amp;menú:</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.cpp" line="28"/>
+        <source>None</source>
+        <translation>Desactivar</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.cpp" line="29"/>
+        <source>Video</source>
+        <translation>Vídeo</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.cpp" line="30"/>
+        <source>Audio</source>
+        <translation>Audio</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.cpp" line="31"/>
+        <source>Video and audio</source>
+        <translation>Vídeo y audio</translation>
+    </message>
+    <message>
+        <location filename="../playerdialog.cpp" line="33"/>
+        <source>Default</source>
+        <translation>Predeterminada</translation>
+    </message>
+</context>
+</TS>
