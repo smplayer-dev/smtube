e5d0be398b02344f8c6b303b631b49ba714874c7
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2021-04-08 14:11:55 +0200

    Check git if the svn revision is not found

--- get_svn_revision.sh
+++ get_svn_revision.sh
@@ -6,16 +6,25 @@
 
 test "$1" && extra="-$1"
 
+PREFIX="SVN"
 svn_revision=`LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
 test $svn_revision || svn_revision=`cd "$1" && grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
 test $svn_revision || svn_revision=`cd "$1" && sed -n -e '/^dir$/{n;p;q}' .svn/entries 2>/dev/null`
 test $svn_revision || svn_revision=0UNKNOWN
 
-NEW_REVISION="#define SVN_REVISION \"SVN-r${svn_revision}${extra}\""
+if test "$svn_revision" = "0UNKNOWN"; then
+    git_revision=`git rev-list --count HEAD`
+    if [ -n "$git_revision" ]; then
+        PREFIX="GIT"
+        svn_revision=$git_revision
+    fi
+fi
+
+NEW_REVISION="#define SVN_REVISION \"$PREFIX-r${svn_revision}${extra}\""
 OLD_REVISION=`cat src/svn_revision.h 2> /dev/null`
 
 # Update version.h only on revision changes to avoid spurious rebuilds
 if test "$NEW_REVISION" != "$OLD_REVISION"; then
     echo "$NEW_REVISION" > src/svn_revision.h
-    echo "SVN-r${svn_revision}${extra}" > svn_revision
+    echo "$PREFIX-r${svn_revision}${extra}" > svn_revision
 fi
