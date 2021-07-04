8df29a0303186e9be421a87de9b51f0059437cc3
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2021-07-04 08:03:35 +0200

    Update compile_mac.sh

--- compile_mac.sh
+++ compile_mac.sh
@@ -2,9 +2,7 @@
 ./get_svn_revision.sh
 
 cd src
+lrelease smtube.pro
 qmake DEFINES+="NO_DEBUG_ON_CONSOLE NO_USE_PLAYERS"
 make -j4
-lrelease smtube.pro
-mkdir smtube.app/Contents/MacOS/translations
-cp translations/*.qm smtube.app/Contents/MacOS/translations/
 macdeployqt smtube.app
